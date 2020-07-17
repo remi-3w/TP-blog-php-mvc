2dab77dd1dce886d832867dbbc33824489369117
Rémi <48048835+remi-3w@users.noreply.github.com>
2020-07-17 11:02:15 +0200

    first commit

---
+++ Engine/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BlogPhp\Engine;
+
+final class Config
+{
+    // Identifiants Database
+    const
+    DB_HOST = 'localhost',
+    DB_NAME = 'blog-mvc',
+    DB_USR = 'root',
+    DB_PWD = '',
+
+    // Titre du site
+    SITE_NAME = 'Esteban a perdu son doudou';
+}
